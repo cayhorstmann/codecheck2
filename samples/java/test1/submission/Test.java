9cf94eacdb00d33f84d73b9dffbaff3c35febf84
Cay Horstmann <cay@horstmann.com>
2015-02-15 12:22:01 +0100

    ...

--- samples/java/test1/submission/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test
-{
-    public static void main(String[] args)
-    {
-        String name = "Udacity";
-        String last = "y";
-        System.out.println(last);
-    }
-}
--- samples/java/test4/submission/Average.java
+++
@@ -1,7 +0,0 @@
-public class Average
-{
-    public double average(int a, int b, int c)
-    {
-        return (a + b + c) / 3;
-    }
-}
