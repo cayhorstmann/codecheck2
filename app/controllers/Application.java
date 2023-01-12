00c24674a59b1a840f57b57709d45b8a059a137b
Cay Horstmann <cay@horstmann.com>
2023-01-12 18:20:17 +0100

    Deploy release play-codecheck

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,15 +13,16 @@ public class Application extends Controller {
     public Result health(Http.Request request) {
        try {
           String df = Util.runProcess("/bin/df /", 1000);
-          String mp = Util.runProcess("/usr/bin/mpstat 2 1", 3000);
           Matcher matcher = dfPattern.matcher(df);
           if (matcher.matches()) {
              String percent =  matcher.group("percent");
              if (Integer.parseInt(percent) > 95)
                 return internalServerError("disk " + percent + "% full");
+             Runtime rt = Runtime.getRuntime();
+             double mem = rt.freeMemory() * 100.0 / rt.totalMemory();
              // TODO: Analyze output
              // http://stackoverflow.com/questions/9229333/how-to-get-overall-cpu-usage-e-g-57-on-linux
-             return ok("CodeCheck\n" + df + "\n" + mp);
+             return ok("CodeCheck\n" + df + "\n" + mem + "% JVM memory free");
           }
           else return ok("df output doesn't match pattern: " + df);
        } catch (Throwable ex) {
