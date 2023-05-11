c55a432b5ace4cf3b0b3b20659f00e452b7be0a7
Cay Horstmann <cay@horstmann.com>
2023-05-11 15:33:37 +0200

    Deploy staging play-codecheck

--- public/workAssignment.js
+++ public/workAssignment.js
@@ -61,7 +61,7 @@ window.addEventListener('DOMContentLoaded', () => {
   function adjustDocHeight(iframe, newHeight) {
     // console.log({frame: iframeKey.get(iframe), oldHeight: iframe.scrollHeight, newHeight })
     if ('chrome' in window) { // https://stackoverflow.com/questions/4565112/javascript-how-to-find-out-if-the-user-browser-is-chrome
-      const CHROME_FUDGE = 16 // to prevent scroll bars in Chrome
+      const CHROME_FUDGE = 32 // to prevent scroll bars in Chrome
       newHeight += CHROME_FUDGE
 	}
     if (iframe.scrollHeight < newHeight)
